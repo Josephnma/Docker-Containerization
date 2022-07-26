98debe510c7d44a018135b02143f7ea91aa3ab2c
Josephnma <Joseph.nma@decagon.dev>
2022-07-26 17:05:38 +0100

    Changed Test File

--- src/test/java/com/blogapp/blogapp/BlogappApplicationTests.java
+++ src/test/java/com/blogapp/blogapp/BlogappApplicationTests.java
@@ -1,13 +1,13 @@
-package com.blogapp.blogapp;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BlogappApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.blogapp.blogapp;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BlogappApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
